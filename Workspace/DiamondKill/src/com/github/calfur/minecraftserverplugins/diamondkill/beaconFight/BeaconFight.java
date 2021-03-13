686a6fae203a90e70b4b89376067f19ee23d2a31
Calfur <robin.nater@hotmail.com>
2021-03-13 13:46:59 +0100

    beaconfight start message

--- Workspace/DiamondKill/src/com/github/calfur/minecraftserverplugins/diamondkill/beaconFight/BeaconFight.java
+++ Workspace/DiamondKill/src/com/github/calfur/minecraftserverplugins/diamondkill/beaconFight/BeaconFight.java
@@ -1,6 +1,13 @@
 package com.github.calfur.minecraftserverplugins.diamondkill.beaconFight;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.bukkit.Bukkit;
+
+import com.github.calfur.minecraftserverplugins.diamondkill.Main;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class BeaconFight {
 	private LocalDateTime startTime;
@@ -16,6 +23,26 @@ public class BeaconFight {
 	
 	public BeaconFight(LocalDateTime startTime) {
 		this.startTime = startTime;
+		long ticksTillStart = ChronoUnit.SECONDS.between(LocalDateTime.now(), startTime)*20;
+		if(ticksTillStart > 0) {
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() {					
+				@Override
+				public void run() {
+					startBeaconFightEvent();
+				}					
+			}, ticksTillStart);
+		}else {
+			startBeaconFightEvent();
+		}
+	}
+	
+	private void startBeaconFightEvent() {
+		Bukkit.broadcastMessage(ChatColor.MAGIC + "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		Bukkit.broadcastMessage(" ");
+		Bukkit.broadcastMessage(ChatColor.BOLD + "Der Beaconevent startet jetzt!");
+		Bukkit.broadcastMessage("In den nächsten " + durationInMinutes + "min können eure Beacons geklaut werden");
+		Bukkit.broadcastMessage(" ");
+		Bukkit.broadcastMessage(ChatColor.MAGIC + "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 	}
 
 }
