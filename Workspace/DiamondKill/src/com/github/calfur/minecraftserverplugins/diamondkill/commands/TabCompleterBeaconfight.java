1f30262388855706fdc3e889f48125d5ee08ef6d
Calfur <robin.nater@hotmail.com>
2021-08-07 15:47:26 +0200

    fix delete suggestions for beaconfight tabCompleter

--- Workspace/DiamondKill/src/com/github/calfur/minecraftserverplugins/diamondkill/commands/TabCompleterBeaconfight.java
+++ Workspace/DiamondKill/src/com/github/calfur/minecraftserverplugins/diamondkill/commands/TabCompleterBeaconfight.java
@@ -12,15 +12,14 @@ public class TabCompleterBeaconfight extends TabCompleterBase{
 	@Override
 	List<String> getSuggestions(String[] previousParameters, Player sender) {
 		List<String> completions = new ArrayList<String>();
-		if(sender.hasPermission("Admin")) {				
-		
+		if(sender.hasPermission("Admin")) {
 			switch (previousParameters.length) {
 			case 1:
 				completions.add("delete");
 				completions.add("add");
 				break;
 			case 2: // DateTime (yyyy-MM-dd_HH:mm)
-				if(previousParameters[0].equals("add") || previousParameters[0].equals("delete")) {
+				if(previousParameters[0].equals("add")) {
 					completions.add(LocalDateTime.now().plusHours(2).withMinute(0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm")));
 					completions.add(LocalDateTime.now().plusHours(1).withMinute(30).format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm")));
 					completions.add(LocalDateTime.now().plusHours(1).withMinute(0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm")));
@@ -28,14 +27,14 @@ public class TabCompleterBeaconfight extends TabCompleterBase{
 				}
 				break;
 			case 3: // Eventduration (Minutes)
-				if(previousParameters[0].equals("add") || previousParameters[0].equals("delete")) {
+				if(previousParameters[0].equals("add")) {
 					completions.add("120");
 					completions.add("90");
 					completions.add("60");
 				}
 				break;
 			case 4: // Attackduration (Minutes)
-				if(previousParameters[0].equals("add") || previousParameters[0].equals("delete")) {
+				if(previousParameters[0].equals("add")) {
 					completions.add("20");
 					completions.add("15");
 					completions.add("10");
