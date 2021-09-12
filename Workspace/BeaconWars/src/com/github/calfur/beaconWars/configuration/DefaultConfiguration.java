23b751f46f77083efb007c82c1f61bc5de736e5d
Calfur <robin.nater@hotmail.com>
2021-09-12 21:57:31 +0200

    Add configuration file

---
+++ Workspace/BeaconWars/src/com/github/calfur/beaconWars/configuration/DefaultConfiguration.java
@@ -0,0 +1,7 @@
+package com.github.calfur.beaconWars.configuration;
+
+public class DefaultConfiguration {
+	public static final String pluginName = "BeaconWars";	
+	public static final String pluginFolder = "plugins//" + pluginName;
+	public static final String databaseFolder = pluginFolder + "//Database";
+}
