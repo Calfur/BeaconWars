2b145743796e806fa482dfc62cce8e9b071bd2aa
Calfur <robin.nater@hotmail.com>
2021-09-19 23:26:52 +0200

    create config file if does not exist

--- Workspace/BeaconWars/src/com/github/calfur/beaconWars/configuration/ConfigFileManager.java
+++ Workspace/BeaconWars/src/com/github/calfur/beaconWars/configuration/ConfigFileManager.java
@@ -1,6 +1,7 @@
 package com.github.calfur.beaconWars.configuration;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -8,7 +9,17 @@ import org.bukkit.configuration.file.YamlConfiguration;
 public class ConfigFileManager {
 
 	private File configFile = new File(ConstantConfiguration.pluginFolder, "config.yml");
-	private FileConfiguration configFileConfiguration = YamlConfiguration.loadConfiguration(configFile);
+	private FileConfiguration configFileConfiguration;
+	
+	public ConfigFileManager() {
+		try {
+			configFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		configFileConfiguration = YamlConfiguration.loadConfiguration(configFile);
+	}
 	
 	public DynamicConfiguration getDynamicConfiguration() {
 		return new DynamicConfiguration(
