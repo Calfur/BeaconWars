2f9f0483540a01940e96785b303aa09124fc0990
Calfur <robin.nater@hotmail.com>
2021-10-21 00:33:54 +0200

    Make transaction command accessible through console

--- Workspace/BeaconWars/src/com/github/calfur/beaconWars/commands/CommandTransaction.java
+++ Workspace/BeaconWars/src/com/github/calfur/beaconWars/commands/CommandTransaction.java
@@ -8,12 +8,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import com.github.calfur.beaconWars.Main;
 import com.github.calfur.beaconWars.Reward;
 import com.github.calfur.beaconWars.RewardManager;
+import com.github.calfur.beaconWars.configuration.ConstantConfiguration;
 import com.github.calfur.beaconWars.database.PlayerDbConnection;
+import com.github.calfur.beaconWars.database.PlayerJson;
 import com.github.calfur.beaconWars.database.TeamDbConnection;
 import com.github.calfur.beaconWars.database.TeamJson;
 import com.github.calfur.beaconWars.database.TransactionDbConnection;
@@ -28,40 +29,37 @@ public class CommandTransaction implements CommandExecutor {
 	private RewardManager rewardManager = Main.getInstance().getRewardManager();
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(sender instanceof Player) {
-			Player executor = (Player)sender;						
-			if(args.length >= 1) {
-				String subCommand = args[0].toLowerCase();
-				switch(subCommand) {
-					case "info":
-						return doTransactionInfo(executor, args);
-					case "list":
-						return doTransactionList(executor, args);
-					case "add": 
-						if(executor.hasPermission("admin")) {	
-							return doAddReward(executor, args);
-						}else {
-							executor.sendMessage(StringFormatter.error("Fehlende Berechtigung für diesen Command"));
-							return true;
-						}
-					case "reverse":
-						if(executor.hasPermission("admin")) {	
-							return doReverse(executor, args);
-						}else {
-							executor.sendMessage(StringFormatter.error("Fehlende Berechtigung für diesen Command"));
-							return true;
-						}
-					default:
-						executor.sendMessage(StringFormatter.error(subCommand + " ist kein vorhandener Subcommand"));
-						return false;
-				}
-			}			
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {				
+		if(args.length >= 1) {
+			String subCommand = args[0].toLowerCase();
+			switch(subCommand) {
+				case "info":
+					return doTransactionInfo(sender, args);
+				case "list":
+					return doTransactionList(sender, args);
+				case "add": 
+					if(sender.hasPermission("admin")) {	
+						return doAddReward(sender, args);
+					}else {
+						sender.sendMessage(StringFormatter.error("Fehlende Berechtigung für diesen Command"));
+						return true;
+					}
+				case "reverse":
+					if(sender.hasPermission("admin")) {	
+						return doReverse(sender, args);
+					}else {
+						sender.sendMessage(StringFormatter.error("Fehlende Berechtigung für diesen Command"));
+						return true;
+					}
+				default:
+					sender.sendMessage(StringFormatter.error(subCommand + " ist kein vorhandener Subcommand"));
+					return false;
+			}
 		}
 		return false;
 	}
 
-	private boolean doTransactionList(Player executor, String[] args) {
+	private boolean doTransactionList(CommandSender sender, String[] args) {
 		int page;
 		int lastAvailablePage = transactionDbConnection.getAmountOfAvailablePages();
 		if(args.length == 1) {
@@ -70,23 +68,23 @@ public class CommandTransaction implements CommandExecutor {
 			try {
 				page = Integer.parseInt(args[1]);
 			}catch (Exception e) {
-				executor.sendMessage(StringFormatter.error("Der Parameter Page muss dem typ int entsprechen"));
+				sender.sendMessage(StringFormatter.error("Der Parameter Page muss dem typ int entsprechen"));
 				return false;
 			}
 			if(page < 1 || page > lastAvailablePage) {
-				executor.sendMessage(StringFormatter.error("Seite " + page + " wurde nicht gefunden. Es sind " + lastAvailablePage + " Seiten verfügbar"));
+				sender.sendMessage(StringFormatter.error("Seite " + page + " wurde nicht gefunden. Es sind " + lastAvailablePage + " Seiten verfügbar"));
 				return false;
 			}
 		}else {		
-			executor.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
+			sender.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
 			return false;
 		}
-		sendTransactionList(executor, page);
+		sendTransactionList(sender, page);
 		return true;
 	}
 
-	private void sendTransactionList(Player executor, int page) {
-		executor.sendMessage(ChatColor.BOLD + "Seite " + page + " von " + transactionDbConnection.getAmountOfAvailablePages() + ":");
+	private void sendTransactionList(CommandSender sender, int page) {
+		sender.sendMessage(ChatColor.BOLD + "Seite " + page + " von " + transactionDbConnection.getAmountOfAvailablePages() + ":");
 		
 		int firstTransactionToShow = page * 10 - 9;
 		int lastTransactionToShow = page * 10;
@@ -94,7 +92,7 @@ public class CommandTransaction implements CommandExecutor {
 			if(transactionDbConnection.existsTransaction(i)) {				
 				TransactionJson transactionJson = transactionDbConnection.getTransaction(i);
 				ChatColor teamColor = getTeamColor(transactionJson.getTeam());
-				executor.sendMessage( 
+				sender.sendMessage( 
 						ChatColor.GOLD + "Id: " + ChatColor.RESET + i + 
 						ChatColor.GOLD + ", Zeit: " + ChatColor.RESET + transactionJson.getDateTime().format(DateTimeFormatter.ofPattern("HH:mm")) + 
 						ChatColor.GOLD + ", Spieler: " + teamColor + transactionJson.getPlayerName() + ChatColor.RESET + 
@@ -114,57 +112,62 @@ public class CommandTransaction implements CommandExecutor {
 		return teamColor;
 	}
 	
-	private boolean doTransactionInfo(Player executor, String[] args) {
+	private boolean doTransactionInfo(CommandSender sender, String[] args) {
 		if(args.length != 2) {
-			executor.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
+			sender.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
 			return false;
 		}
 		int transactionId;
 		try {
 			transactionId = Integer.parseInt(args[1]);
 		}catch(NumberFormatException e) {
-			executor.sendMessage(StringFormatter.error("Der Id Parameter muss dem Typ Int entsprechen"));
+			sender.sendMessage(StringFormatter.error("Der Id Parameter muss dem Typ Int entsprechen"));
 			return false;
 		}
 		if(!transactionDbConnection.existsTransaction(transactionId)) {
-			executor.sendMessage(StringFormatter.error("Diese Transaktion ist nicht registriert"));
+			sender.sendMessage(StringFormatter.error("Diese Transaktion ist nicht registriert"));
 			return false;
 		}
 		TransactionJson transactionJson = transactionDbConnection.getTransaction(transactionId);
 		if(!teamDbConnection.existsTeam(transactionJson.getTeam())) {
-			executor.sendMessage(StringFormatter.error("Dieses Teams ist nicht registriert"));
+			sender.sendMessage(StringFormatter.error("Dieses Teams ist nicht registriert"));
 			return false;
 		}
 		TeamJson teamJson = teamDbConnection.getTeam(transactionJson.getTeam());
-		executor.sendMessage(StringFormatter.bold("Id: " + transactionId));
-		executor.sendMessage(ChatColor.GOLD + "Zeit: " + ChatColor.RESET + transactionJson.getDateTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))); 
-		executor.sendMessage(ChatColor.GOLD + "Spieler: " + teamJson.getColor() + transactionJson.getPlayerName());
-		executor.sendMessage(ChatColor.GOLD + "Team: " + teamJson.getColor() + transactionJson.getTeam());
-		executor.sendMessage(ChatColor.GOLD + "Transferierte Diamanten: " + ChatColor.AQUA + transactionJson.getTransactedDiamonds());
-		executor.sendMessage(ChatColor.GOLD + "Transferierte Punkte: " + ChatColor.RESET + transactionJson.getTransactedPoints());
-		executor.sendMessage(ChatColor.GOLD + "Grund: " + ChatColor.RESET + transactionJson.getReason());
+		sender.sendMessage(StringFormatter.bold("Id: " + transactionId));
+		sender.sendMessage(ChatColor.GOLD + "Zeit: " + ChatColor.RESET + transactionJson.getDateTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))); 
+		sender.sendMessage(ChatColor.GOLD + "Spieler: " + teamJson.getColor() + transactionJson.getPlayerName());
+		sender.sendMessage(ChatColor.GOLD + "Team: " + teamJson.getColor() + transactionJson.getTeam());
+		sender.sendMessage(ChatColor.GOLD + "Transferierte Diamanten: " + ChatColor.AQUA + transactionJson.getTransactedDiamonds());
+		sender.sendMessage(ChatColor.GOLD + "Transferierte Punkte: " + ChatColor.RESET + transactionJson.getTransactedPoints());
+		sender.sendMessage(ChatColor.GOLD + "Grund: " + ChatColor.RESET + transactionJson.getReason());
 		return true;
 	}
 	
-	private boolean doAddReward(Player executor, String[] args) {
+	private boolean doAddReward(CommandSender sender, String[] args) {
 		if(args.length < 5) {
-			executor.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
+			sender.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
 			return false;
 		}
 		int amount;
 		try {
 			amount = Integer.parseInt(args[2]);
 		}catch(NumberFormatException e) {
-			executor.sendMessage(StringFormatter.error("Der Parameter 'amount' muss dem Typ Int entsprechen"));
+			sender.sendMessage(StringFormatter.error("Der Parameter 'amount' muss dem Typ Int entsprechen"));
 			return false;
 		}
 		if(amount == 0) {
-			executor.sendMessage(StringFormatter.error("Der Parameter 'amount' darf nicht 0 sein"));
+			sender.sendMessage(StringFormatter.error("Der Parameter 'amount' darf nicht 0 sein"));
 			return false;
 		}
 		String name = args[3];
 		if(!playerDbConnection.existsPlayer(name)) {
-			executor.sendMessage(StringFormatter.error("Dieser Spieler ist nicht vorhanden"));
+			sender.sendMessage(StringFormatter.error("Dieser Spieler ist nicht vorhanden"));
+			return false;
+		}
+		PlayerJson playerJson = playerDbConnection.getPlayer(name);
+		if(playerJson.getTeamId() == ConstantConfiguration.spectatorTeamNumber) {
+			sender.sendMessage(StringFormatter.error("Dem Spectator " + name + " kann keine Belohnung ausgezahlt werden"));
 			return false;
 		}
 		ArrayList<String> reasonWords = new ArrayList<>(Arrays.asList(args));
@@ -182,23 +185,23 @@ public class CommandTransaction implements CommandExecutor {
 			rewardManager.addReward(name, new Reward(0, amount), reason);
 			break;
 		default:
-			executor.sendMessage(StringFormatter.error(subCommand + " ist kein vorhandener Subcommand"));
+			sender.sendMessage(StringFormatter.error(subCommand + " ist kein vorhandener Subcommand"));
 			return false;
 		}
-		executor.sendMessage(ChatColor.GREEN + "Transaktion erfolgreich ausgeführt");
+		sender.sendMessage(ChatColor.GREEN + "Transaktion erfolgreich ausgeführt");
 		return true;
 	}
 
-	private boolean doReverse(Player executor, String[] args) {
+	private boolean doReverse(CommandSender sender, String[] args) {
 		if(args.length < 3) {
-			executor.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
+			sender.sendMessage(StringFormatter.error("Der Command enthält nicht die richtige Anzahl Parameter"));
 			return false;
 		}
 		int transactionId;
 		try {
 			transactionId = Integer.parseInt(args[1]);
 		}catch(NumberFormatException e) {
-			executor.sendMessage(StringFormatter.error("Der Parameter Id muss dem Typ Int entsprechen"));
+			sender.sendMessage(StringFormatter.error("Der Parameter Id muss dem Typ Int entsprechen"));
 			return false;
 		}
 		ArrayList<String> reasonWords = new ArrayList<>(Arrays.asList(args));
@@ -206,7 +209,7 @@ public class CommandTransaction implements CommandExecutor {
 		reasonWords.remove(0); // Id
 		String reason = String.join(" ", reasonWords);
 		if(!transactionDbConnection.existsTransaction(transactionId)) {
-			executor.sendMessage(StringFormatter.error("Diese Transaktion ist nicht registriert"));
+			sender.sendMessage(StringFormatter.error("Diese Transaktion ist nicht registriert"));
 			return false;
 		}
 		TransactionJson transactionJson = transactionDbConnection.getTransaction(transactionId);
@@ -217,7 +220,7 @@ public class CommandTransaction implements CommandExecutor {
 					-transactionJson.getTransactedPoints()
 				), 
 				"Die Transaktion " + transactionId + " wurde rückgängig gemacht, weil: " + reason);
-		executor.sendMessage(ChatColor.GREEN + "Transaktion erfolgreich rückgängig gemacht");
+		sender.sendMessage(ChatColor.GREEN + "Transaktion erfolgreich rückgängig gemacht");
 		return true;
 	}
 }
