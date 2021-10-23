51fb689c06b473c0db8e82f81d4f71df9d7d2a8d
Calfur <robin.nater@hotmail.com>
2021-10-23 16:30:07 +0200

    HungerGames only if the project is not in startmode

--- Workspace/BeaconWars/src/com/github/calfur/beaconWars/hungerGamesLootDrop/ItemSpawnAnnouncer.java
+++ Workspace/BeaconWars/src/com/github/calfur/beaconWars/hungerGamesLootDrop/ItemSpawnAnnouncer.java
@@ -4,15 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 import com.github.calfur.beaconWars.Main;
+import com.github.calfur.beaconWars.commands.CommandProjectStart;
 import com.github.calfur.beaconWars.configuration.IConfiguration;
 import com.github.calfur.beaconWars.helperClasses.StringFormatter;
 
 public class ItemSpawnAnnouncer implements Runnable{
 	IConfiguration configuration = Main.getInstance().getConfiguration();
+	private CommandProjectStart commandProjectStart = Main.getInstance().getCommandProjectStart();
 	
 	@Override
 	public void run() {
-		if(configuration.areHungerGamesEnabled()) {	
+		if(configuration.areHungerGamesEnabled() && commandProjectStart.isProjectActive()) {	
 			Bukkit.broadcastMessage(ChatColor.GOLD + "In 5 Minuten spawnt in der Mitte ein " + ChatColor.AQUA + "Diamant" + ChatColor.GOLD + ", falls mindestens " + StringFormatter.teamWord(configuration.getHungerGamesMinimumAmountOfRequiredOnlineTeams()) + " online " + StringFormatter.singularOrPlural(configuration.getHungerGamesMinimumAmountOfRequiredOnlineTeams(), "ist.", "sind."));
 			Bukkit.broadcastMessage(ChatColor.GOLD + "Benutze den Befehl " + ChatColor.WHITE + "/compass spawn" + ChatColor.GOLD + " um einen Kompass dorthin zu bekommen");
 		}
